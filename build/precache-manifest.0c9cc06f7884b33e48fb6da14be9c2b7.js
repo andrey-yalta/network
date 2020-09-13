bc89da17cfd52f5e9aaca1fa1631a9535e720714
andrey-yalta <andrey.yalta.96qx@gmail.com>
2020-09-13 07:21:58 +0300

    build new version

---
+++ build/precache-manifest.0c9cc06f7884b33e48fb6da14be9c2b7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cac22647254882cb9eb4",
+    "url": "/network/static/css/main.c9279644.chunk.css"
+  },
+  {
+    "revision": "cac22647254882cb9eb4",
+    "url": "/network/static/js/main.cac22647.chunk.js"
+  },
+  {
+    "revision": "a26e9e049afb0823fdc1",
+    "url": "/network/static/css/1.90a19403.chunk.css"
+  },
+  {
+    "revision": "a26e9e049afb0823fdc1",
+    "url": "/network/static/js/1.a26e9e04.chunk.js"
+  },
+  {
+    "revision": "1673ba35ff4a2b0103fa",
+    "url": "/network/static/css/2.626ca80d.chunk.css"
+  },
+  {
+    "revision": "1673ba35ff4a2b0103fa",
+    "url": "/network/static/js/2.1673ba35.chunk.js"
+  },
+  {
+    "revision": "b03cc0ac5807edbb304b",
+    "url": "/network/static/js/3.b03cc0ac.chunk.js"
+  },
+  {
+    "revision": "a1b47797717a4d7d2396",
+    "url": "/network/static/js/runtime~main.a1b47797.js"
+  },
+  {
+    "revision": "83427d44bd4aa94ea76d34915e16f63a",
+    "url": "/network/static/media/user.83427d44.png"
+  },
+  {
+    "revision": "1181204bc0ef21c53bdd85ad29782e62",
+    "url": "/network/static/media/circles.1181204b.svg"
+  },
+  {
+    "revision": "d6c7b98b897a3c0b1a53c95173537e82",
+    "url": "/network/index.html"
+  }
+];
