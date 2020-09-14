5be1434a861c79c9c8a6b0460d1132f8eaf25257
andrey-yalta <andrey.yalta.96qx@gmail.com>
2020-09-14 06:43:45 +0300

    add hash router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter, Route, withRouter} from "react-router-dom";
+import {HashRouter, Route, withRouter} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
@@ -12,16 +12,10 @@ import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/preloader/preloader";
 import store from "./redux/redux-store";
-// import Settings from "./components/Settings/Settings";
-// import Music from "./components/Music/Music";
 import News from "./components/News/News"
 import {Switch} from "react-router-dom"
-
 const Music = React.lazy(() => import("./components/Music/Music"));
-// const News = React.lazy(()=>import("./components/News/News"));
 const Settings = React.lazy(() => import("./components/Settings/Settings"));
-
-// const DialogsContainer = React.lazy(()=>import("./components/Dialogs/DialogsContainer"));
 
 class App extends Component {
     catchAllUnhandledErrors = (promiseRejectionEvent)=>{
@@ -46,45 +40,15 @@ class App extends Component {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Switch>
-                    {/*<Route exact path='/'*/}
-                    {/*       render={() => <Redirect to={"/profile"}/> }/>*/}
-
-                    <Route path='/dialogs'
-                           render={() => <DialogsContainer/>}/>
-
-                    <Route path='/profile/:userId?'
-                           render={() => <ProfileContainer/>}/>
-
-                    <Route path='/users'
-                           render={() => <UsersContainer/>}/>
-
-                    {/*<Route  path='/login/hello'*/}
-                    {/*       render={() => <div>hello</div>}/>*/}
-
-                    <Route  path='/login'
-                           render={() => <LoginPage/>}/>
-
-
-
-                    <Route path='/news'
-                           render={() => {
-                               return <React.Suspense fallback={<div>loading...</div>}>
-                                   <News/>
-                               </React.Suspense>}}/>
-                    <Route path='/music'
-                           render={() => {
-                               return <React.Suspense fallback={<div>loading...</div>}>
-                                   <Music/>
-                               </React.Suspense>}}/>
-                    <Route path='/settings'
-                           render={() => {
-                               return <React.Suspense fallback={<div>loading...</div>}>
-                                   <Settings/>
-                               </React.Suspense>}}/>
-
-
-                    <Route  path='*'
-                           render={() => <div> 404</div>}/>
+                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                    <Route path='/users' render={() => <UsersContainer/>}/>
+                    <Route  path='/login' render={() => <LoginPage/>}/>
+                    <Route path='/news' render={() => {return <React.Suspense fallback={<div>loading...</div>}><News/></React.Suspense>}}/>
+                    <Route path='/music' render={() => { return <React.Suspense fallback={<div>loading...</div>}><Music/></React.Suspense>}}/>
+                    <Route path='/settings' render={() => {return <React.Suspense fallback={<div>loading...</div>}><Settings/></React.Suspense>}}/>
+                    {/*<Route  path='*'*/}
+                    {/*       render={() => <div> 404</div>}/>*/}
                     </Switch>
                 </div>
             </div>
@@ -101,10 +65,11 @@ let AppContainer = compose(
     connect(mapStateToProps, {initializeApp}))(App);
 
 const SamuraiJsApp = (props) => {
-    return <BrowserRouter>
+    return <HashRouter>
+
         <Provider store={store}>
             <AppContainer/>
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 }
 export default SamuraiJsApp;
